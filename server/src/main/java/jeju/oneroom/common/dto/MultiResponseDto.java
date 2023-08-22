8f4b23e45b0261b35a7af8f3b5614f62316552f5
bangjaeyoung <qkdwo4613@gmail.com>
2023-08-22 20:56:19 +0900

    [BE] refactor - 응답 데이터 dto 클래스 수정

--- server/src/main/java/jeju/oneroom/common/dto/MultiResponseDto.java
+++ server/src/main/java/jeju/oneroom/common/dto/MultiResponseDto.java
@@ -5,15 +5,19 @@ import org.springframework.data.domain.Page;
 
 import java.util.List;
 
-// Page가 첨가된 Response 객체
 @Getter
 public class MultiResponseDto<T> {
-    private List<T> data;
-    private PageInfo pageInfo;
+
+    private final List<T> data;
+    private final PageInfo pageInfo;
 
     public MultiResponseDto(Page<T> page) {
         this.data = page.getContent();
-        this.pageInfo = new PageInfo(page.getNumber() + 1,
-                page.getSize(), page.getTotalElements(), page.getTotalPages());
+        this.pageInfo = new PageInfo(
+            page.getNumber() + 1,
+            page.getSize(),
+            page.getTotalElements(),
+            page.getTotalPages()
+        );
     }
 }
--- server/src/main/java/jeju/oneroom/common/dto/PageInfo.java
+++ server/src/main/java/jeju/oneroom/common/dto/PageInfo.java
@@ -3,10 +3,10 @@ package jeju.oneroom.common.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-// 페이지 정보
-@AllArgsConstructor
 @Getter
+@AllArgsConstructor
 public class PageInfo {
+
     private int page;
     private int size;
     private long totalElements;
