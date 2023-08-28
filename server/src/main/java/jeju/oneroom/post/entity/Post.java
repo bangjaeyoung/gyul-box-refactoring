ee0e7938c345640a7cd9b6c0c3f21f4bc96c6bff
bangjaeyoung <qkdwo4613@gmail.com>
2023-08-29 00:17:22 +0900

    [BE] fix - 단일 게시글 조회 시, 조회 수 증가 로직 최종 수정

--- server/src/main/java/jeju/oneroom/post/entity/Post.java
+++ server/src/main/java/jeju/oneroom/post/entity/Post.java
@@ -76,19 +76,13 @@ public class Post extends BaseEntity {
         this.content = content;
     }
 
-    // 동일 유저 검증
-
-    public boolean isAuthor(User user) {
-        return this.user.equals(user);
-    }
-
-
-
+    // 조회 수 1 증가
     public void increaseViews() {
         this.views++;
     }
 
-
-
-
+    // 동일 유저 검증
+    public boolean isAuthor(User user) {
+        return this.user.equals(user);
+    }
 }
