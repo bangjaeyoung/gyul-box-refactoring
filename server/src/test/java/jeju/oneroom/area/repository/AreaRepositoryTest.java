ff31b49daad86b3e8395e2a11e151ea769355d1d
bangjaeyoung <qkdwo4613@gmail.com>
2023-10-10 22:43:13 +0900

    [BE] test - Area repository 단위테스트 코드 수정

--- server/src/test/java/jeju/oneroom/area/repository/AreaRepositoryTest.java
+++ server/src/test/java/jeju/oneroom/area/repository/AreaRepositoryTest.java
@@ -8,36 +8,64 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ActiveProfiles("test")
 @SpringBootTest
+@ActiveProfiles("test")
 class AreaRepositoryTest {
 
     @Autowired
     private AreaRepository areaRepository;
 
-    @DisplayName("지역 이름을 통해 단일 지역을 조회한다.")
+    @DisplayName("지역 이름을 통해 단일 지역을 조회한다. 존재한다면 지역을 반환한다.")
     @Test
-    void findByAreaName() {
+    void findByAreaName_Exists() {
         // given
-        Coordinate coordinate = Coordinate.builder()
-            .latitude(11.1111)
-            .longitude(22.2222)
-            .build();
+        Coordinate coordinate = createCoordinate();
 
-        Area area = Area.builder()
-            .areaCode(1L)
-            .areaName("가나읍")
-            .coordinate(coordinate)
-            .build();
+        Area area = createArea(1L, "가나읍");
 
         areaRepository.save(area);
 
         // when
-        Area foundArea = areaRepository.findByAreaName("가나읍");
+        Optional<Area> result = areaRepository.findByAreaName("가나읍");
 
         // then
-        assertThat(foundArea.getAreaName()).isEqualTo("가나읍");
+        assertThat(result).isPresent();
+        assertThat(result.get().getAreaName()).isEqualTo("가나읍");
+    }
+
+    @DisplayName("지역 이름을 통해 단일 지역을 조회한다. 존재하지 않는다면, 빈 객체가 반환된다.")
+    @Test
+    void findByAreaName_NotFound() {
+        // given
+        Coordinate coordinate = createCoordinate();
+
+        Area area = createArea(1L, "가나읍");
+
+        areaRepository.save(area);
+
+        // when
+        Optional<Area> result = areaRepository.findByAreaName("가나");
+
+        // then
+        assertThat(result).isEmpty();
+    }
+
+    private Area createArea(Long areaCode, String areaName) {
+        return Area.builder()
+            .areaCode(areaCode)
+            .areaName(areaName)
+            .coordinate(createCoordinate())
+            .build();
+    }
+
+    private Coordinate createCoordinate() {
+        return Coordinate.builder()
+            .latitude(11.1111)
+            .longitude(22.2222)
+            .build();
     }
 }
