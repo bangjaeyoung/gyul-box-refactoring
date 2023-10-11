656ec590a5cb5dba64255e2f27d04d2c05191c13
bangjaeyoung <qkdwo4613@gmail.com>
2023-10-11 17:38:02 +0900

    [BE] test - Area mapper 테스트 코드 작성을 위한 JPA Auditing 분리

--- server/src/main/java/jeju/oneroom/OneroomApplication.java
+++ server/src/main/java/jeju/oneroom/OneroomApplication.java
@@ -2,14 +2,11 @@ package jeju.oneroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class OneroomApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OneroomApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(OneroomApplication.class, args);
+    }
 }
